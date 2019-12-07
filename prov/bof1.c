efbfc0314dfff0dc19c24433c5f10ae5e4aeeea2
Imreplay <imreplay@imreplay.com>
2019-12-08 02:23:55 +0900

    Update bof1.c

--- prov/bof1.c
+++ prov/bof1.c
@@ -14,6 +14,7 @@ int main(int argc, char const *argv[]){
    {
         printf("Good Skill!\n");
         printf("Answer is FLAG{Network_security_is_So_Fun!}\n");
+        fflush(stdout);
    }
 
 }
