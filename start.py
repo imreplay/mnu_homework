19565dfc52801fbf556314e7d1cc4f1505f04f93
Imreplay <imreplay@imreplay.com>
2019-12-08 02:31:28 +0900

    Update start.py

--- start.py
+++ start.py
@@ -7,7 +7,15 @@ import os
 os.system("adduser --disabled-login  --home /home/pwn/level1 --uid 4001 --gecos ''  level1")
 
 #level file setting
-os.system("cp ./prov/bof1* /home/pwn/level1/")
+os.system("cp ./prov/bof1 /home/pwn/level1/")
+f = open("./prov/bof1.c","r")
+data = f.read()
+f.close()
+data.replace("Network_security_is_So_Fun!","???")
+f = open("./prov/bof1.c","w")
+f.write(data)
+f.close()
+os.system("cp ./prov/bof1.c /home/pwn/level1/")
 os.system("chown :level1 /home/pwn/level1/bof1;chmod 111 /home/pwn/level1/bof1")
 
 #install xinetd
