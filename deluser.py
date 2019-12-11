07a286c3560ea0a5d438e09b20532b43e0ce889f
Imreplay <imreplay@imreplay.com>
2019-12-11 09:27:31 +0900

    Update deluser.py

--- deluser.py
+++ deluser.py
@@ -1,5 +1,4 @@
 import os
-for i in range(4001,4005):
-    os.system("deluser --remove-home level{}".format(i-4000))
+os.system("deluser --remove-home level1")
 
 os.system("sed -ie '$d' /etc/services")
